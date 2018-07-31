0f9fa8da11378d18eaa099904fd70d665aac9f28
toiye <toiye@DESKTOP-BJB9GPG>
2018-07-31 16:37:00 +0700

    Movie Controller And Custom View

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using System.Data.Entity;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var moves = _context.Movies.Include(g => g.Genre).ToList();
+            return View(moves);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(g => g.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
+        }
+    }
+}
