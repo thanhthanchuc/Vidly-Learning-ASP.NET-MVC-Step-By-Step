dd63682d06ebf232baa819bfd4a9943eaf694e21
toiye <toiye@DESKTOP-BJB9GPG>
2018-07-31 15:01:59 +0700

    Create CustomersController

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //Just use one time.
+        protected override void Dispose(bool disposing)
+        {
+           _context.Dispose();
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customer = _context.Customers.ToList();
+            return View(customer);
+        }
+    }
+}
